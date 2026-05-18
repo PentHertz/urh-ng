a96b2438b2c843f68f6c0aee9c1d830bf4d2fab1
Willem Melching <willem.melching@gmail.com>
2026-05-18 17:06:51 +0200

    minimal diff

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except ImportError:
     sys.exit(1)
 
 
-def _load_module(name, relpath):
+def _load_from_path(name, relpath):
     abspath = os.path.join(os.path.dirname(os.path.abspath(__file__)), relpath)
     spec = importlib.util.spec_from_file_location(name, abspath)
     module = importlib.util.module_from_spec(spec)
@@ -26,10 +26,11 @@ def _load_module(name, relpath):
     return module
 
 
-ExtensionHelper = _load_module(
+ExtensionHelper = _load_from_path(
     "urh_build.ExtensionHelper", "src/urh/dev/native/ExtensionHelper.py"
 )
 COMPILER_DIRECTIVES = ExtensionHelper.COMPILER_DIRECTIVES
+version = _load_from_path("urh_build.version", "src/urh/version.py")
 
 if sys.platform == "win32":
     OPEN_MP_FLAG = "/openmp"
@@ -40,6 +41,14 @@ elif sys.platform == "darwin":
 else:
     OPEN_MP_FLAG = "-fopenmp"
     NO_NUMPY_WARNINGS_FLAG = "-Wno-cpp"
+
+UI_SUBDIRS = ("actions", "delegates", "views")
+PLUGINS = [
+    path
+    for path in os.listdir("src/urh/plugins")
+    if os.path.isdir(os.path.join("src/urh/plugins", path))
+]
+URH_DIR = "urh"
 
 IS_RELEASE = os.path.isfile(os.path.join(tempfile.gettempdir(), "urh_releasing"))
 
@@ -72,6 +81,32 @@ class build_ext(_build_ext):
         import numpy
 
         self.include_dirs.append(numpy.get_include())
+
+
+def get_packages():
+    packages = [URH_DIR]
+    separator = os.path.normpath("/")
+    for dirpath, dirnames, filenames in os.walk(os.path.join("./src/", URH_DIR)):
+        package_path = os.path.relpath(
+            dirpath, os.path.join("./src/", URH_DIR)
+        ).replace(separator, ".")
+        if len(package_path) > 1:
+            packages.append(URH_DIR + "." + package_path)
+
+    return packages
+
+
+def get_package_data():
+    package_data = {"urh.cythonext": ["*.pyx", "*.pxd"]}
+    for plugin in PLUGINS:
+        package_data["urh.plugins." + plugin] = ["*.ui", "*.txt"]
+
+    package_data["urh.dev.native.lib"] = ["*.pyx", "*.pxd"]
+
+    if IS_RELEASE and sys.platform == "win32":
+        package_data["urh.dev.native.lib.shared"] = ["*.dll", "*.txt"]
+
+    return package_data
 
 
 def get_extensions():
@@ -110,12 +145,51 @@ def get_extensions():
     return extensions
 
 
-extra_package_data = {}
-if IS_RELEASE and sys.platform == "win32":
-    extra_package_data["urh.dev.native.lib.shared"] = ["*.dll", "*.txt"]
+def read_long_description():
+    try:
+        with open("README.md") as f:
+            text = f.read()
+        return text
+    except:
+        return ""
+
+
+install_requires = ["numpy<3.0", "psutil", "cython", "setuptools"]
+if IS_RELEASE:
+    install_requires.append("PyQt6")
+else:
+    try:
+        import PyQt6
+    except ImportError:
+        install_requires.append("PyQt6")
 
 setup(
+    name="urh-ng",
+    version=version.VERSION,
+    description="URH-NG: Universal Radio Hacker - Next Gen. Investigate wireless protocols like a boss.",
+    long_description=read_long_description(),
+    long_description_content_type="text/markdown",
+    author="Sebastien Dudek",
+    author_email="sdudek@penthertz.com",
+    maintainer="Sebastien Dudek (PentHertz)",
+    maintainer_email="sdudek@penthertz.com",
+    package_dir={"": "src"},
+    package_data=get_package_data(),
+    url="https://github.com/PentHertz/URH",
+    license="GNU General Public License (GPL)",
+    download_url="https://github.com/PentHertz/URH/tarball/v" + str(version.VERSION),
+    install_requires=install_requires,
+    setup_requires=["numpy<3.0"],
+    python_requires=">=3.9",
+    packages=get_packages(),
     ext_modules=get_extensions(),
     cmdclass={"build_ext": build_ext},
-    package_data=extra_package_data,
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "urh-ng = urh.main:main",
+            "urh = urh.main:main",
+            "urh_cli = urh.cli.urh_cli:main",
+        ]
+    },
 )
